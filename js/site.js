acb1ff9de0bff34340098a99f0bd4278c35e7c94
k-nandini <kuman293@newschool.edu>
2023-10-16 00:53:22 -0400

    V1

---
+++ js/site.js
@@ -0,0 +1,23 @@
+
+    const ring = {
+        material: "Gold",
+        gemstone: "Yellow Sapphire",
+        size: 5, // Ring size (US ring size)
+        price: 3000, // Price in Dollars
+        weight: 8, // Weight in grams
+        isAntique: false, 
+        manufacturer: "XYZ Jewelers",
+        description: "A stunning gold ring with a sapphire center stone.",
+        hallmark: "14K", // Material purity hallmark
+        isCustomMade: true, 
+        productionYear: 2017, // Year it was produced
+        gemstoneCarat: 0.75, // Carat weight of stone
+        hasCertificate: true, // Certificate of authenticity
+        makingCharges: 1000, // Making charges in Dollars
+        isResizable: false, //Whether it can be resized
+      };
+      
+      // Accessing properties:
+      console.log("Material:", ring.material);
+      console.log("Price:", ring.price);
+      console.log("Gemstone:", ring.gemstone);
